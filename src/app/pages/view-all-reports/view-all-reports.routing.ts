fb1c09ef679493dea008a1911ee70d9afd7d57b5
ricky_xia <rickyx@sedata.com>
2020-11-02 14:32:44 -0500

    feat: viewsummaryreport: -improve UI

--- src/app/pages/view-all-reports/view-all-reports.routing.ts
+++ src/app/pages/view-all-reports/view-all-reports.routing.ts
@@ -2,11 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { ViewSummaryReportComponent } from './view-summary-report/view-summary-report.component';
 import { ViewCustomReportComponent } from './view-custom-report/view-custom-report.component';
 import { ViewPlReportComponent } from './view-pl-report/view-pl-report.component';
+import { AuthGuard } from '../../_helpers';
 
 const childRoutes: Routes = [
-  { path: 'view-summary-report', component: ViewSummaryReportComponent},
-  { path: 'view-custom-report', component: ViewCustomReportComponent},
-  { path: 'view-pl-report', component: ViewPlReportComponent}
+  { path: 'view-summary-report', component: ViewSummaryReportComponent, canActivate: [AuthGuard] },
+  { path: 'view-custom-report', component: ViewCustomReportComponent, canActivate: [AuthGuard] },
+  { path: 'view-pl-report', component: ViewPlReportComponent, canActivate: [AuthGuard] }
 ];
 
 export const routing = RouterModule.forChild(childRoutes);
